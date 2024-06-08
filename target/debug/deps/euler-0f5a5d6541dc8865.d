5f56741acb29a03814df7b36d3d3a55592e2b9d1
Pluto28 <mendax103@gmail.com>
2024-06-08 20:34:43 -0300

    remove useless

--- target/debug/deps/euler-0f5a5d6541dc8865.d
+++
@@ -1,8 +0,0 @@
-/home/DonQuixote/Documents/sftw/euler/target/debug/deps/euler-0f5a5d6541dc8865: src/main.rs src/id_0.rs src/id_1.rs src/id_2.rs
-
-/home/DonQuixote/Documents/sftw/euler/target/debug/deps/euler-0f5a5d6541dc8865.d: src/main.rs src/id_0.rs src/id_1.rs src/id_2.rs
-
-src/main.rs:
-src/id_0.rs:
-src/id_1.rs:
-src/id_2.rs:
